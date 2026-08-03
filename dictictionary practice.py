3b8fcd35ffe676fed5d942ae017d78835e9bbc6d
andre1-afx <andrew5121224@gmail.com>
2026-08-03 18:03:26 +0800

    dictionary practice

---
+++ dictictionary practice.py
@@ -0,0 +1,24 @@
+n = int(input())
+score = {}
+
+for _ in range(n):
+    name, status = input().split()
+    if status=="AC":
+       score[name]=score.get(name,0)+1
+       
+    
+
+max_score = 0
+ans = ""
+
+for name in score:
+    if score.get(name)>max_score:
+       max_score=score[name]
+       ans=name;
+
+print(ans)
+
+   
+     
+        
+
